a0fa31a2c281b268bd6f9fc97e79e2346d8fbcd9
Kirill Abramov <kirill.is.ninja.frontend@gmail.com>
2020-04-29 23:26:22 +0300

    Updates

---
+++ precache-manifest.b3307dedc6d5e2345e20da1a8a8a45f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae7590ff7bfcb5f05df20b4fbf2a8bc2",
+    "url": "/github-api/index.html"
+  },
+  {
+    "revision": "ea01cf343af6258c1f18",
+    "url": "/github-api/static/js/2.ff512914.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-api/static/js/2.ff512914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9dff983f9604479750f",
+    "url": "/github-api/static/js/main.96d5d5a8.chunk.js"
+  },
+  {
+    "revision": "70365ca46cf9e3a7dbfc",
+    "url": "/github-api/static/js/runtime-main.57547e3f.js"
+  }
+]);
